99815e0fdd99fcc9dcc3dcc365fe12400729ce78
Juan Carlos <juancarlos.rabasco@gmail.com>
2022-12-09 09:26:59 +0100

    primer commit

---
+++ Class1.java
@@ -0,0 +1,8 @@
+public class Class1 {
+    public static void main(String[] args){
+        System.out.println("-----------------");
+        System.out.println("Esto es una prueba");
+        System.out.println("-----------------");
+
+    }
+}
