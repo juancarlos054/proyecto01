533c057014b3c0459a0bb2f218d536681b5f3df7
Juan Carlos <juancarlos.rabasco@gmail.com>
2022-12-09 09:41:44 +0100

    3er commit

--- Class1.java
+++ Class1.java
@@ -2,7 +2,6 @@ public class Class1 {
     public static void main(String[] args){
         System.out.println("-----------------");
         System.out.println("Esto es una prueba");
-        System.out.println("-----------------");
         System.out.println("Esto es una segunda prueba");
 
     }
